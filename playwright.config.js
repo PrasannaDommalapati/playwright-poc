38e25b0b3921219228859d5ff3108aa81932f0df
PDommalapati <prasanna.dommalapati@gardinerbros.co.uk>
2025-02-20 23:43:19 +0000

    playwright config

---
+++ playwright.config.js
@@ -0,0 +1,29 @@
+// playwright.config.js
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',  // Directory where test files are stored
+  timeout: 30000,      // Test timeout (30 seconds)
+  retries: 2,          // Retry failed tests twice
+  use: {
+    headless: true,   // Run tests in a visible browser
+    viewport: { width: 1280, height: 720 }, // Set browser viewport size
+    actionTimeout: 5000,  // Timeout for each action (5 seconds)
+    trace: 'on',      // Collect trace when running tests
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' },
+    }
+    // ,
+    // {
+    //   name: 'firefox',
+    //   use: { browserName: 'firefox' },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { browserName: 'webkit' },
+    // },
+  ],
+});
--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
-const { readExcelFile } = require('./index');
+const { readExcelFile } = require('../index');
 
 test('test', async ({ page }) => {
     const sheetName = 'Sheet1';  // Specify the sheet name
-    const data = readExcelFile('data.xlsx', sheetName);  // Call the function to read the Excel file
+    const data = readExcelFile('date2.xlsx', sheetName);  // Call the function to read the Excel file
 
     // Log "First Name" from each row
+    console.log(data);
     data.forEach(row => {
         console.log(row["First Name"]);  // Log the "First Name" value
     });
